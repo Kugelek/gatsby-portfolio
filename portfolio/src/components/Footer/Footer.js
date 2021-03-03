979727b757661a17bf26cfbd9c8e73076b007f5a
Kugelek <poblocki.jakub@gmail.com>
2021-03-04 00:24:06 +0100

    footer

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './Footer.modules.scss';
+import Logo from '../../images/logo.png';
 const Footer = () => {
-  return <div>yyy</div>;
+  return (
+    <footer className="footer">
+      <img className="footer__logo" src={Logo} alt="poblocki dev footer logo" />
+      <div className="footer__contact">
+        <span className="footer__icon"></span>
+        <span className="footer__icon"></span>
+      </div>
+      <div className="footer__copyrights">
+        Copyrights © {new Date().getFullYear()} Jakub Pobłocki. All rights
+        reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
