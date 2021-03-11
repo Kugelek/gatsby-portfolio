d04a7a772f937d57c71e337759a7d078fb289d9f
Kugelek <poblocki.jakub@gmail.com>
2021-03-11 15:05:17 +0100

    projects section

--- portfolio/src/components/ProjectsList/ProjectItem.js
+++ portfolio/src/components/ProjectsList/ProjectItem.js
@@ -17,20 +17,30 @@ const ProjectItem = ({ projectData, photos }) => {
       />
       <div className="projectitem__details">
         <h1 className="projectitem__heading">{projectData.name}</h1>
-        <p className="projectitem__description">
+        <p
+          data-aos-offset="100"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="700"
+          className="projectitem__description"
+        >
           {projectData.descriptionShort + '... '}
         </p>
-        <TechStackPills techs={projectData.techStack} />
-        <a href="#">
-          <img
-            className="projectitem__details-icon"
-            src={DetailsIcon}
-            alt="project details"
-          />
-        </a>
-        <a href={projectData.github}>
-          <img className="projectitem__github-icon" src={GhIcon} alt="github" />
-        </a>
+        <TechStackPills
+          className="projectitem__pills"
+          techs={projectData.techStack}
+        />
+        <div className="projectitem__iconbox">
+          <a href="#">
+            <img
+              className="projectitem__icon"
+              src={DetailsIcon}
+              alt="project details"
+            />
+          </a>
+          <a href={projectData.github}>
+            <img className="projectitem__icon" src={GhIcon} alt="github" />
+          </a>
+        </div>
       </div>
     </article>
   );
